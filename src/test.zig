e100a81f6c8e47a10dc08bf388ad23328cd0f035
Aidan Pinard <aidan@aidanpinard.co>
2023-08-14 17:50:15 -0400

    added (failing) tests for ldx

--- src/test.zig
+++ src/test.zig
@@ -2,5 +2,5 @@ comptime {
     _ = @import("./tests/cpu_execution.zig");
     _ = @import("./tests/jsr.zig");
     _ = @import("./tests/lda.zig");
-    // _ = @import("./tests/ldx.zig");
+    _ = @import("./tests/ldx.zig");
 }
